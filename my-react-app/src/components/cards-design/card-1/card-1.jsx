fa5292d34902df0706c1e806e8aee446262549a6
Sanket_Godse <sanketgodse059@gmail.com>
2023-12-05 00:23:02 +0530

    cards css file added

--- my-react-app/src/components/cards-design/card-1/card-1.jsx
+++ my-react-app/src/components/cards-design/card-1/card-1.jsx
@@ -2,18 +2,7 @@ import React from 'react';
 import '../card-1/card-1.css'; // Import the CSS file for styling
 import C1_1 from "./image/c1_1.jpg" 
 const ResponsiveImage = () => {
-    const FamilyData = [
-        { id: 1, title: 'Husband', content: 'Lorem ipsum dolor sit amet.'  },
-        { id: 2, title: 'Wife', content: 'Consectetur adipiscing elit.' },
-        { id: 3, title: 'H-Father', content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-        { id: 4, title: 'H-Mother', content: 'Ut enim ad minim veniam.' },
-        { id: 5, title: 'W-Father', content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-        { id: 6, title: 'W-Mother', content: 'Ut enim ad minim veniam.' },
-        // { id: 7, title: 'GrandFather', content: 'Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
-        { id: 8, title: 'Date', content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' },
-        { id: 9, title: 'Location', content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' },
-        { id: 10, title: 'Date', content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' },
-      ];
+
   return (
     <div className='card-1-main-container'>
     <div className="container">
--- my-react-app/src/components/hero/Hero.jsx
+++ my-react-app/src/components/hero/Hero.jsx
@@ -1,12 +1,13 @@
 // HeroComponent.jsx
 import React from 'react';
 import './hero.css'; // Import your styles
+import '../cards-design/commen-css/cards.css'; // Import your styles
 
 function Hero() {
   return (
     <div className="hero-container">
     <div className="hero-content">
-      <h1>E-निमंत्रण </h1>
+      <h1 className='card-font'>E-निमंत्रण </h1>
       <h2 className='hh2'>Lets invite digitally</h2>
       <button>Get Started</button>
     </div>
