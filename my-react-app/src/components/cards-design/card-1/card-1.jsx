99c4f8e3230d79ecabab1fab28cf65f95e9811d7
Sanket_Godse <sanketgodse059@gmail.com>
2023-12-05 13:15:45 +0530

    worked on card 1

--- my-react-app/src/components/cards-design/card-1/card-1.jsx
+++ my-react-app/src/components/cards-design/card-1/card-1.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
-import '../card-1/card-1.css'; // Import the CSS file for styling
-import C1_1 from "./image/c1_1.jpg" 
+import '../card-1/card-1.css'; // Import the CSS file for styling.
+import Husband from "./image/Husband.jpg"
+import Wife from "./image/Wife.jpg"
 const ResponsiveImage = () => {
 
   return (
-    <div className='card-1-main-container'>
-    <div className="container">
-      <img src={C1_1} alt="Your Image" className="pop-up-image" />
-      <div className="content">
-        <h1>Your Header</h1>
-        <p>
-          Your smallng to do  around 50 words here.
-        </p>
-      </div>
-    </div>    <div className="container">
-      <img src={C1_1} alt="Your Image" className="pop-up-image" />
-      <div className="content">
-        <h1>Your Header</h1>
-        <p>
-          Your small paragraph with around 50 words goes here. Your small paragraph with around 50
-          words goes here. Your small paragraph with around 50 words goes here.
-        </p>
-      </div>
-    </div>
+    <div className='card-1-sec'>
+      {/* <div className='sec-2'>
+        <h1 className='sec-2-head'></h1>
+        <p className='sec-2-para'></p>
+      </div> */}
+      <div className='sec-1'>
+          <div className='sec-1-head'>
+              <h1 className='sec-1-header' >वर-वधू</h1>
+              <p>भाविका व प्रशांत च्या लग्नाकडे, डोळे लागले जगाचे… <br />
+                आहेरात आणा फक्त, अनमोल गिफ्ट आशीर्वादाचे…</p>
+          </div>
+          <div className='couple'>
+              <div className='hsbnd'>
+                  <div className='couple-img-sec'>
+                      <img className='couple-img' src={Wife} alt="" />
+                  </div>
+                  <h1 className='sec-1-header'>चि. सौ. कां. भाविका</h1>
+                  <p className=''>कै.श्री. मिलिंद रमण खैरनार यांची द्वितीय कन्या <br /> रा. पिंपळनेर, ह.मु. कळवा, ठाणे</p>
+              </div>
+              <div className='wfi'>
+                  <div className='couple-img-sec'>
+                      <img className='couple-img' src={Husband} alt="" />
+                  </div>
+                  <h1 className='sec-1-header'>चि. प्रशांत</h1>
+                  <p className=''>श्री. मोहन आनंदा पवार यांचे चतुर्थ चिरंजीव ,<br /> रा. नंदुरबार</p>
+              </div>
+          </div>
+          </div>
     </div>
   );
 };
