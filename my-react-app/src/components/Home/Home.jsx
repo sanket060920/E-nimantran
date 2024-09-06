f335acbdb30c2201f6652672d42ee28901c97a84
Sanket_Godse <sanketgodse059@gmail.com>
2024-09-06 11:44:00 +0530

    home page added

---
+++ my-react-app/src/components/Home/Home.jsx
@@ -0,0 +1,27 @@
+import '../card-1/card-1.css'; 
+import '../common/cards.css'; 
+
+
+const Card1 = () => {
+  return (
+    <>
+    hello from home
+    {/* <FirstCmp/>
+    <Muscicmp/>
+    <SecondCmp/>
+    <SuvarnaKshanCmp/>
+    <ThirdCmp/>
+    <SaptapadiCmp/>
+    <Gallerycmp/>
+    <SheduleCmp/>
+    <Timercmp/>
+    <FourthCmp/>
+    <Footer/> */}
+
+    </>
+
+  )
+};
+
+
+export default Card1;
