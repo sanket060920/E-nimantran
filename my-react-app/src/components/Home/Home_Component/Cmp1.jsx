7c85fddc8af7f129005125cf690f6b028112da2a
Sanket_Godse <sanketgodse059@gmail.com>
2024-09-07 21:33:01 +0530

    plan description added

--- my-react-app/src/components/Home/Home_Component/Cmp1.jsx
+++ my-react-app/src/components/Home/Home_Component/Cmp1.jsx
@@ -1,5 +1,6 @@
 import '../Home_Component/Cmp1.css'
 
+
 const Cmp1 = () => {
     return (
       <div id='Main_Div'>
@@ -9,41 +10,41 @@ const Cmp1 = () => {
         <div id='Second_Div'>
           <div id='Plan_Div'>
               <div id='Plan_heading'>
-                <h1 >Basic plan</h1>
+                <h1 >Start-Up plan</h1>
               </div>
               <div id='Plan_description'>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
+                <h4 class='Plan_point'>-Facebook</h4>
+                <h4 class='Plan_point'>-Instagram</h4>
+                <h4 class='Plan_point'>-Google my Business</h4>
+                <h4 class='Plan_point'>-Dedicated Manager</h4>
+                <h4 class='Plan_point'>-Influencers Videos</h4>
+                <h4 class='Plan_point'> </h4>                
               </div>
           </div>
           <div id='Plan_Div'>
             <div id='Plan_heading'>
-                <h1 >Business plan</h1>
+                <h1 >Professional plan</h1>
               </div>
               <div id='Plan_description'>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
+                <h4 class='Plan_point'>-All in Start-up plan</h4>
+                <h4 class='Plan_point'>-Facebook ADS</h4>
+                <h4 class='Plan_point'>-Influencers Videos</h4>
+                <h4 class='Plan_point'>-Weekly Reports + Strategy</h4>
+                <h4 class='Plan_point'>-In-house Content Creation</h4>
+                <h4 class='Plan_point'> </h4>                
               </div>
           </div>
           <div id='Plan_Div'>
               <div id='Plan_heading'>
-                <h1 >Premium plan</h1>
+                <h1 >Master plan</h1>
               </div>
               <div id='Plan_description'>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
-                <h4 class='Plan_point'>-Lorem ipsum dolor sit amet.</h4>
+                <h4 class='Plan_point'>-All in Professional Plan</h4>
+                <h4 class='Plan_point'>-Youtube</h4>
+                <h4 class='Plan_point'>-SEO</h4>
+                <h4 class='Plan_point'>-Influencers Vedios</h4>
+                <h4 class='Plan_point'>-Linkedin</h4>
+                <h4 class='Plan_point'>-More Features and Services</h4>
               </div>
           </div>
         </div>
