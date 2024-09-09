f65e3edd881022a20618821328ac5f3b44b23de8
Sanket_Godse <sanketgodse059@gmail.com>
2024-09-09 20:39:17 +0530

    resume file location change

--- my-react-app/src/components/Home/Portfolio/Components/Resume.jsx
+++ my-react-app/src/components/Home/Portfolio/Components/Resume.jsx
@@ -8,10 +8,12 @@ const Resume = () => {
   return (
     <div className="profile-container">
       <NavBar/>
-      <h1>Download My Resume</h1>
-      <a href="my-react-app\src\components\Home\Portfolio\Files\Resume(Sanket Godse).txt" download>
-        <button className="cv-button">Download CV</button>
-      </a>
+      <div className="Resume_content">
+        <h1>Download My Resume</h1>
+        <a href="my-react-app\public\Files\Resume(Sanket Godse).txt" download>
+            <button className="cv-button">Download CV</button>
+        </a>
+      </div>
       <BottomFooter/>
       {/* my-react-app\src\components\Home\Portfolio\Files\Resume(Sanket Godse).txt */}
 
